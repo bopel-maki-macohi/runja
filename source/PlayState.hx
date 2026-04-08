0a2696414e2fe2ebd56256d1df104d351eba8178
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 07:23:42 -0500

    player and floor + unfunctional jumping

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,16 +1,33 @@
 package;
 
+import flixel.util.FlxColor;
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
 
 class PlayState extends FlxState
 {
+	public var _player:FlxSprite;
+	public var _floor:FlxSprite;
+
 	override public function create()
 	{
 		super.create();
+
+		_floor = new FlxSprite(0, FlxG.height / 2);
+		_floor.makeGraphic(FlxG.width, Math.round(_floor.y), FlxColor.GRAY);
+		add(_floor);
+
+		_player = new FlxSprite();
+		_player.makeGraphic(32, 32, FlxColor.RED);
+		add(_player);
 	}
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
+
+		if (FlxG.keys.justReleased.SPACE)
+			_player.velocity.y -= 10;
 	}
 }
