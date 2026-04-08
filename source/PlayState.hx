8474a529962033456b9c172b45ff973be25d5084
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 11:11:21 -0500

    max jumps

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -23,7 +23,7 @@ class PlayState extends FlxState
 
 		_player = new FlxSprite();
 		_player.makeGraphic(32, 32, FlxColor.RED);
-		
+
 		_player.maxVelocity.set(80, 200);
 		_player.acceleration.y = 200;
 		_player.drag.x = _player.maxVelocity.x * 4;
@@ -31,18 +31,25 @@ class PlayState extends FlxState
 		add(_player);
 	}
 
+	var playerJumps:Int = 0;
+	var playerJumpsMax:Int = 1;
+
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
 		FlxG.collide(_player, _floor, onFloor);
-		
-		if (FlxG.keys.justReleased.SPACE)
+
+		if (FlxG.keys.justReleased.SPACE && playerJumps < playerJumpsMax)
+		{
 			_player.velocity.y = -_player.maxVelocity.y / 2;
+			playerJumps++;
+		}
 	}
 
 	function onFloor(o1, o2)
 	{
 		_player.velocity.y = 0;
+		playerJumps = 0;
 	}
 }
