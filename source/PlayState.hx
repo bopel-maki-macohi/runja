493857de08efc75ee60e42dab9f617876ff037e8
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-08 07:30:33 -0500

    fix jumping (thx EZPlatformer haxeflixel showcase)

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,7 @@
 package;
 
+import flixel.util.FlxCollision;
+import flixel.math.FlxMath;
 import flixel.util.FlxColor;
 import flixel.FlxG;
 import flixel.FlxSprite;
@@ -20,14 +22,23 @@ class PlayState extends FlxState
 
 		_player = new FlxSprite();
 		_player.makeGraphic(32, 32, FlxColor.RED);
+		
+		_player.maxVelocity.set(80, 200);
+		_player.acceleration.y = 200;
+		_player.drag.x = _player.maxVelocity.x * 4;
+
 		add(_player);
 	}
+
+	var player_accelValue = 100;
 
 	override public function update(elapsed:Float)
 	{
 		super.update(elapsed);
 
 		if (FlxG.keys.justReleased.SPACE)
-			_player.velocity.y -= 10;
+			_player.velocity.y = -_player.maxVelocity.y / 2;
+
+		FlxG.collide(_player, _floor);
 	}
 }
